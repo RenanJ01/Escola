60b1575dc2cab58775ef5f90bf687d00f644b119
RenanJ01 <jonatasrenan02@gmail.com>
2023-03-16 02:02:50 -0300

    up tp02

--- DSW/TP02/resultado.php
+++ DSW/TP02/resultado.php
@@ -48,16 +48,43 @@ function test_input($data)
 <html lang="pt">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="tp02.css">
-  <title>TP - Formulario - Resultado</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="tp02.css">
+    <title>TP - Formulario - Resultado</title>
 </head>
 
 <body>
-  <section>
-    <?php
+
+    <header>
+        <nav class="nav_menu" id="topnav">
+            <a href="#" class="active">Home</a>
+            <a href="\DSW/menuSub.html">SubMenu - DSW</a>
+            <a href="#form_quest">Formulario - Questão</a>
+            <a href="resultado.php">Formulario - Resposta</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()"> <i class="fa fa-bars"></i>
+            </a>
+        </nav>
+
+        <script>
+        function myFunction() {
+            var x = document.getElementById("topnav");
+            if (x.className === "nav_menu") {
+                x.className += " responsive";
+            } else {
+                x.className = "nav_menu";
+            }
+        }
+        </script>
+        
+        <div class="hdr_ctn_img">
+            <img class="hdr_img" src="img/hdr_img.jpg" alt="Cidade Urbana">
+        </div>
+    </header>
+
+    <section>
+        <?php
     echo "<h2>Informações:</h2>
       <p>Nome: $name<br>
       E-mail: $email<br>
@@ -66,17 +93,17 @@ function test_input($data)
       Jogos: $jogos<br>
       </p>"
     ?>
-    <br><br>
-    <?php
+        <br><br>
+        <?php
     echo "<h2>Jogos:</h2>"  
 
     ?>
-  </section>
-  <footer>
-    <p class="ftr_direitos"> TP - Curriculos - 2022, todos os
-      direitos reservados.</p>
-    <p class="ftr_autor">Desenvolvido por: <a href="#" title="Jonatas e Eriel">Jonatas Renan e Eriel</a></p>
-  </footer>
+    </section>
+    <footer>
+        <p class="ftr_direitos"> TP - Curriculos - 2022, todos os
+            direitos reservados.</p>
+        <p class="ftr_autor">Desenvolvido por: <a href="#" title="Jonatas e Eriel">Jonatas Renan e Eriel</a></p>
+    </footer>
 </body>
 
 </html>
